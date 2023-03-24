707f7f6b3bd648f19fd690b8b755fe4df4b41864
springin98 <114986489+springin98@users.noreply.github.com>
2023-03-24 11:31:38 +0900

    [level 0] Title: 약수 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
@@ -1,4 +1,3 @@
 def solution(n):
-    list = ([i for i in range(1, n+1) if n%i == 0])
-    list.sort()
-    return list
+    return ([i for i in range(1, n+1) if n%i == 0])
+    # list.sort() 어차피 1부터 순서대로 넣어지기 때문에 굳이 sort할 필요가 없다.
