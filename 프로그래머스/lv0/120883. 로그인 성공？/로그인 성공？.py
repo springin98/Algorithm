7893cbe2c6efe313c31683de6027d5fc716721c9
springin98 <114986489+springin98@users.noreply.github.com>
2023-03-31 13:00:23 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,9 +1,6 @@
 def solution(id_pw, db):
-    for i in db :
-        if i[0] == id_pw[0] :
-            if i[1] == id_pw[1] :
-                return "login"
-            else :
-                return "wrong pw"
+    if dict(db).get(id_pw[0]) :
+        db_pw = dict(db).get(id_pw[0])
+        return "login" if db_pw == id_pw[1] else "wrong pw"
     
     return "fail"
