4f7a4e66411725ddbeb26f67e059adca3eff756c
msdio <rlagofka1@gmail.com>
2023-04-02 20:23:53 +0900

    pg 161990

---
+++ msdio/python/pg_161990.py
@@ -0,0 +1,11 @@
+def solution(wallpaper):
+    x = []
+    y = []
+    
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if(wallpaper[i][j] == '#'):
+                x.append(j)
+                y.append(i)
+    
+    return [min(y), min(x), max(y)+1, max(x)+1]
