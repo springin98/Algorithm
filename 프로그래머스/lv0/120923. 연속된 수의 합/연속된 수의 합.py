4585117961fc9bec51cf0429a6852fe5530b3d7c
박보민 <114986489+springin98@users.noreply.github.com>
2023-04-13 00:14:39 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,15 +1,7 @@
-def get_middle_num(num, total):
-    return total // num
-
-
 def solution(num, total):
-    middle_idx = num // 2
+    mid = total//num
     
-    middle_num = get_middle_num(num, total)
-    
-    start_num = middle_idx * - 1 if num % 2 == 1 else middle_idx * -1 + 1
-    end_num = middle_idx + 1
-    
-    answer = [num + middle_num for num in range(start_num, end_num)]
-    
-    return answer
+    if num%2 == 0 :
+        return [i for i in range(mid-num//2+1, mid+num//2+1)]
+    else :
+        return [i for i in range(mid-num//2, mid+num//2+1)]
