920177bca3ef47000857ef029a05139a06034b16
박보민 <114986489+springin98@users.noreply.github.com>
2023-05-18 22:35:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    arr =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    answer = 0
+    
+    for i in arr :
+        if i not in numbers :
+            answer += i
+    
+    return answer
